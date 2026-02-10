9ed108464912a325e124917a0c0079f681ed5d55
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2026-02-10 09:00:21 -0300

    feat: Imports AuthModule in others modules

--- src/modules/barber-service/barber-service.module.ts
+++ src/modules/barber-service/barber-service.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { BarberServiceService } from './barber-service.service';
-import { BarberServiceController } from './barber-service.controller';
+import { Module } from "@nestjs/common";
+import { BarberServiceService } from "./barber-service.service";
+import { BarberServiceController } from "./barber-service.controller";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-  controllers: [BarberServiceController],
-  providers: [BarberServiceService],
+	imports: [AuthModule],
+	controllers: [BarberServiceController],
+	providers: [BarberServiceService],
 })
 export class BarberServiceModule {}
--- src/modules/booking/booking.module.ts
+++ src/modules/booking/booking.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { BookingService } from './booking.service';
-import { BookingController } from './booking.controller';
+import { Module } from "@nestjs/common";
+import { BookingService } from "./booking.service";
+import { BookingController } from "./booking.controller";
+import { AuthModule } from "../auth/auth.module";
 
 @Module({
-  controllers: [BookingController],
-  providers: [BookingService],
+	imports: [AuthModule],
+	controllers: [BookingController],
+	providers: [BookingService],
 })
 export class BookingModule {}
