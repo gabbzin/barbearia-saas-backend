b157ae843c04c41277478660d4d72ed26d84372a
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2026-02-19 14:10:01 -0300

    feat: Add customer record with tenant ID and create decorator for tenant ID.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { All, Controller, Inject, Req, Res } from "@nestjs/common";
+import { All, Body, Controller, Inject, Post, Req, Res } from "@nestjs/common";
 import { AUTH } from "./auth.provider";
 import { Request, Response } from "express";
 import { toNodeHandler } from "better-auth/node";
 import { AuthService } from "./auth.service";
+import { RegisterUserDto } from "./dto/register-user.dto";
+import { TenantId } from "./decorators/tenant-id.decorator";
 
 @Controller("auth")
 export class AuthController {
@@ -11,12 +13,16 @@ export class AuthController {
 		private readonly authService: AuthService
 	) {}
 
+	@Post("register-client")
+	async registerClientInTenant(
+		@Body() body: RegisterUserDto,
+		@TenantId() tenantId: string
+	) {
+		return this.authService.registerClientInTenant(body, tenantId);
+	}
+
 	@All("*")
 	async handleAuth(@Req() req: Request, @Res() res: Response) {
 		return toNodeHandler(this.auth)(req, res);
-	}
-
-	async registerClientInTenant(@Req() req: Request, @Res() res: Response) {
-		return this.authService.registerClientInTenant(req.body);
 	}
 }
